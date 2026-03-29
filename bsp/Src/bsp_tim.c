1250713a766405f22967dd8aed0c4e5a5d3bd5e0
Hanbin Zheng <hanbinzheng54@gmail.com>
2026-03-30 02:36:12 +0800

    chore: update comment

--- bsp/Src/bsp_tim.c
+++ bsp/Src/bsp_tim.c
@@ -2,8 +2,8 @@
 
 void tim_init(void)
 {
-    // enable tim5, tim12, tim15
-    HAL_TIM_Base_Start_IT(&htim5);  // neck control, 1000hz
-    HAL_TIM_Base_Start_IT(&htim12); // head control, 125hz
-    HAL_TIM_Base_Start_IT(&htim15); // chassis control, 125hz
+    /* enable tim5, tim12, tim15 */
+    HAL_TIM_Base_Start_IT(&htim5);  /* for gimbal task, 1000 hz */
+    HAL_TIM_Base_Start_IT(&htim12); /* for armor task, 125 hz */
+    HAL_TIM_Base_Start_IT(&htim15); /* for chassis task, 125 hz */
 }
