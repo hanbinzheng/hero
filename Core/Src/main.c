c65fe9de53d330865b86d04abcbb0f806ee6a608
Hanbin Zheng <hanbinzheng54@gmail.com>
2026-04-10 06:31:06 +0800

    feat: change the system_ready signal logic to guarantee the correctness of imu, remove unnecessary includes

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -35,14 +35,11 @@
 #include "bsp_usart.h"
 
 #include "dbus.h"
-#include "dji_motor.h"
-#include "dm_motor.h"
 #include "imu.h"
+#include "buzzer.h"
+#include "dm_motor.h"
 #include "mi_motor.h"
-
-#include "ammo.h"
-#include "chassis.h"
-#include "gimbal.h"
+#include "dji_motor.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -143,16 +140,15 @@ int main(void)
   imu_init();
   mi_motor_enable();
   dm4310_enable();
+  /* system ready signal and buzzer */
+  system_ready = 1;
+  buzzer_beep_ms(200);
+  dwt_delay_s(4); /* waitting for gravity correction of timu */
+  buzzer_beep_ms(50);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  /* pwm and system ready here */
-  HAL_TIM_PWM_Start(&htim12, TIM_CHANNEL_2);
-  __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_2, 2000);
-  dwt_delay_ms(50);
-  __HAL_TIM_SET_COMPARE(&htim12, TIM_CHANNEL_2, 0);
-  system_ready = 1;
 	while (1) {
 		dwt_delay_ms(1);
 		count++;
